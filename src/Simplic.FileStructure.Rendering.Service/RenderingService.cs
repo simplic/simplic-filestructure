8ecd27bec4d21be2769088d9d7a94b08286b4cd6
Yavuz Günay <yavuzgunay@gmail.com>
2018-11-07 17:05:34 +0100

    return empty class for items

--- src/Simplic.FileStructure.Rendering.Service/RenderingService.cs
+++ src/Simplic.FileStructure.Rendering.Service/RenderingService.cs
@@ -9,7 +9,7 @@ namespace Simplic.FileStructure.Rendering.Service
     public class RenderingService : IRenderingService
     {
         // HTML template to render. must contain {iconClasses}, {content}
-        private string htmlTemplate = "<!DOCTYPE html><html><head> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/> <style>{iconClasses} ul, #myUL{list-style-type: none;}#myUL{margin: 0; padding: 0;}.caret{cursor: pointer; -webkit-user-select: none; /* Safari 3.1+ */ -moz-user-select: none; /* Firefox 2+ */ -ms-user-select: none; /* IE 10+ */ user-select: none;}.caret::before{content: \"\25B6\"; color: black; display: inline-block; margin-right: 6px;}.caret-down::before{-ms-transform: rotate(90deg); /* IE 9 */ -webkit-transform: rotate(90deg); /* Safari */' transform: rotate(90deg);}.nested{display: none;}.active{display: block;}</style></head><body>{content}<script>var toggler=document.getElementsByClassName(\"caret\"); var i; for (i=0; i < toggler.length; i++){toggler[i].addEventListener(\"click\", function(){this.parentElement.querySelector(\".nested\").classList.toggle(\"active\"); this.classList.toggle(\"caret-down\");});}</script></body></html>";
+        private string htmlTemplate = "<!DOCTYPE html><html><head> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/> <style>{iconClasses} ul, #myUL{list-style-type: none;}#myUL{margin: 0; padding: 0;}.caret{cursor: pointer; -webkit-user-select: none; /* Safari 3.1+ */ -moz-user-select: none; /* Firefox 2+ */ -ms-user-select: none; /* IE 10+ */ user-select: none;}.caret::before{content: \"\\25B6\"; color: black; display: inline-block; margin-right: 6px;}.caret-down::before{-ms-transform: rotate(90deg); /* IE 9 */ -webkit-transform: rotate(90deg); /* Safari */' transform: rotate(90deg);}.nested{display: none;}.active{display: block;}</style></head><body>{content}<script>var toggler=document.getElementsByClassName(\"caret\"); var i; for (i=0; i < toggler.length; i++){toggler[i].addEventListener(\"click\", function(){this.parentElement.querySelector(\".nested\").classList.toggle(\"active\"); this.classList.toggle(\"caret-down\");});}</script></body></html>";
         private IDictionary<string, string> icons = new Dictionary<string, string>();
 
         public string Render(FileStructure fileStructure)
@@ -32,7 +32,7 @@ namespace Simplic.FileStructure.Rendering.Service
             foreach (var item in icons)
             {
                 sb.Append($".{item.Key}");
-                sb.Append("{cursor: pointer;-webkit-user-select: none; /* Safari 3.1+ */-moz-user-select: none; /* Firefox 2+ */-ms-user-select: none; /* IE 10+ */user-select: none;}");
+                sb.Append("{ cursor: pointer;-webkit-user-select: none; /* Safari 3.1+ */-moz-user-select: none; /* Firefox 2+ */-ms-user-select: none; /* IE 10+ */user-select: none;}");
                 sb.Append($".{item.Key}::before");
                 sb.Append("{ content: \"" + item.Value + "\";color: black;display: inline - block;margin - right: 6px;}");
             }
@@ -86,7 +86,15 @@ namespace Simplic.FileStructure.Rendering.Service
         /// <returns></returns>
         private string GetIcon(Guid directoryTypeId)
         {
-            return "\\25B6";
+            // TODO: get the icon, convert it to base 64 and add it to the icons list
+            //if (!icons.ContainsKey("caret"))
+            //{             
+            //    icons.Add("caret", "\\25B6");
+            //}
+
+            //return "caret";
+
+            return "";
         }
 
         private bool HasChildren(IList<Directory> directories, Guid id)
