c3c9b24b6869befd2c26b7f22cf9f45894a16c30
beggers <be@spiegelburg.net>
2019-05-06 11:36:59 +0200

    Upgrade version

--- src/Simplic.FileStructure.UI/Properties/AssemblyInfo.cs
+++ src/Simplic.FileStructure.UI/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.19.55")]
-[assembly: AssemblyFileVersion("1.6.19.55")]
+[assembly: AssemblyVersion("1.6.119.56")]
+[assembly: AssemblyFileVersion("1.6.119.56")]
