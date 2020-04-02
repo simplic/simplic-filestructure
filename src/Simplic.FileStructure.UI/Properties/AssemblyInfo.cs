1d543f5bde3e717335d132356e879bc2c2a1a13c
beggers <be@spiegelburg.net>
2020-04-02 20:49:09 +0200

    Update versions 2.0.20.42

--- src/Simplic.FileStructure.UI/Properties/AssemblyInfo.cs
+++ src/Simplic.FileStructure.UI/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.119.924")]
-[assembly: AssemblyFileVersion("1.7.119.924")]
+[assembly: AssemblyVersion("2.0.20.42")]
+[assembly: AssemblyFileVersion("2.0.20.42")]
