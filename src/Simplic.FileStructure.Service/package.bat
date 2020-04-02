058619ffd95e1fe7146d52f42bed8afb7f0d167b
beggers <be@spiegelburg.net>
2020-04-02 20:40:04 +0200

    Remove old packaging

--- src/Simplic.FileStructure.Service/package.bat
+++
@@ -1,3 +0,0 @@
-del *.nupkg
-nuget pack Simplic.FileStructure.Service.csproj -properties Configuration=Debug
-nuget push *.nupkg -Source http://simplic.biz:10380/nuget
--- src/Simplic.FileStructure/package.bat
+++
@@ -1,3 +0,0 @@
-del *.nupkg
-nuget pack Simplic.FileStructure.csproj -properties Configuration=Debug
-nuget push *.nupkg -Source http://simplic.biz:10380/nuget
