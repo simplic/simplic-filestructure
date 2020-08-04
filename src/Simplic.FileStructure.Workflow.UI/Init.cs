cc35e3d4717171364c53880a3a04f55a1fe586b6
tanyel.tuncer@spiegelburg.biz <tanyel.tuncer@spiegelburg.biz>
2020-08-04 14:02:49 +0200

    Added new class libary

---
+++ src/Simplic.FileStructure.Workflow.UI/Init.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.FileStructure.Workflow.UI
+{
+    public static class Init
+    {
+        public static void Initialize()
+        {
+
+        }
+    }
+}
