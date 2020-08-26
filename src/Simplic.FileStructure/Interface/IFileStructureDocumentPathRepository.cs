310fbedd4fa0fe9cfeb91356faf4e9d3aa8931b2
tanyel.tuncer@spiegelburg.biz <tanyel.tuncer@spiegelburg.biz>
2020-08-26 13:18:29 +0200

    Added more comments

--- src/Simplic.FileStructure/Interface/IFileStructureDocumentPathRepository.cs
+++ src/Simplic.FileStructure/Interface/IFileStructureDocumentPathRepository.cs
@@ -15,21 +15,21 @@ namespace Simplic.FileStructure
         /// <param name="documentId">Document id</param>
         /// <returns>Enumerable of paths</returns>
         IEnumerable<FileStructureDocumenPath> GetByDocumentId(Guid documentId);
-        
+
         /// <summary>
         /// Gets all by Directory id 
         /// </summary>
-        /// <param name="directoryId"></param>
-        /// <returns></returns>
+        /// <param name="directoryId">Directory instance id</param>
+        /// <returns>Enumerable of paths</returns>
         IEnumerable<FileStructureDocumenPath> GetByDirectoryId(Guid directoryId);
 
         /// <summary>
-        /// Gets a bool if any of the directory id is protected
-        /// It checks all the guids and if any guid is protected it returns true.
+        /// Goes through a list of directory ids and checks, whether any directory contains a document, that is protected. If a protected documents exists,
+        /// true will be returned. If no protected directory was found, false will be returned
         /// </summary>
-        /// <param name="guids"></param>
-        /// <returns></returns>
-        bool IsProtected(List<Guid> guids);
+        /// <param name="directoryIds">Ids for directory</param>
+        /// <returns>bool</returns>
+        bool IsProtected(IList<Guid> directoryIds);
 
     }
 }
