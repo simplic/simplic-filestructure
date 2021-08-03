af3a149c044e96aff2165732dd8d182c3ff6d76f
tanyel.tuncer <tanyel.tuncer@simplic.biz>
2021-08-03 11:51:58 +0200

    added substitution pack

---
+++ src/Simplic.FileStructure.Workflow.Data.DB/IWorkflowSubstitutionRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Simplic.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.FileStructure.Workflow
+{
+    public class WorkflowSubstitutionRepository : IWorkflowSubstitutionRepository
+    {
+        ISqlService sqlService;
+        public WorkflowSubstitutionRepository(ISqlService sqlService)
+        {
+            this.sqlService = sqlService;
+        }
+
+        public bool SubstitutionExists(long currentUserId, long absenceUserId)
+        {
+            return sqlService.OpenConnection((connection) =>
+            {
+                return connection.QueryFirstOrDefault<bool>("SELECT CASE WHEN EXISTS(SELECT * FROM ESS_MS_Intern_User where " +
+                    "Ident = :userId" +
+                    "SubstituteUserId = :substituteUserId " +
+                    "and SubstitutionIsActive = 1) " +
+                    "THEN 1 " +
+                    "ELSE 0 " +
+                    "END as Result; ",
+                    new { userId = absenceUserId, substituteUserId = currentUserId });
+            });
+        }
+    }
+}
---
+++ src/Simplic.FileStructure.Workflow/Interface/IWorkflowSubstitutionRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.FileStructure.Workflow
+{
+    public interface IWorkflowSubstitutionRepository
+    {
+        /// <summary>
+        /// Checks if the current user has a substitute for the absence user
+        /// </summary>
+        /// <param name="currentUserId">The user id that is the substitute</param>
+        /// <param name="absenceUserId">The user id that is in absence</param>
+        /// <returns></returns>
+        bool SubstitutionExists(long currentUserId, long absenceUserId);
+    }
+}
