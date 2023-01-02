5f9723b18509e1c869b360d18cc1f45c8ac2ac21
tanyel.tuncer <tanyel.tuncer@simplic.biz>
2023-01-02 10:49:35 +0100

    updated version number

--- src/Simplic.FileStructure.UI/Properties/AssemblyInfo.cs
+++ src/Simplic.FileStructure.UI/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.1122.1010")]
-[assembly: AssemblyFileVersion("2.2.1122.1010")]
+[assembly: AssemblyVersion("2.3.1122.1010")]
+[assembly: AssemblyFileVersion("2.3.1122.1010")]
